e8bb8add0a6971a60521de90e54b72ca568bf0a9
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2022-05-30 12:58:33 +0300

    ✨ feat: add the starter site

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,10 @@
 aws cloudformation create-stack \
 	--stack-name static-site-stack
 	--template-body file://website-s3.yaml
+
+cd udacity-starter-website
+
+aws s3api put-object --bucket my-549763406380-bucket --key index.html --body index.html 
+aws s3 cp vendor/ s3://my-549763406380-bucket/vendor/ --recursive
+aws s3 cp css/ s3://my-549763406380-bucket/css/ --recursive
+aws s3 cp img/ s3://my-549763406380-bucket/img/ --recursive
