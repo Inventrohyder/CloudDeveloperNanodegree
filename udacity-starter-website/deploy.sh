b150d9a004ccbd20a7d00669a1d358725524b729
Haitham Alhad Hyder <22187034+Inventrohyder@users.noreply.github.com>
2022-05-30 14:23:54 +0300

    🐛 fix: separate cloud formation from deployment

--- udacity-starter-website/deploy.sh
+++ udacity-starter-website/deploy.sh
@@ -1,10 +1,4 @@
 #! /bin/zsh
-aws cloudformation create-stack \
-	--stack-name static-site-stack
-	--template-body file://website-s3.yaml
-
-cd udacity-starter-website
-
 aws s3api put-object --bucket my-549763406380-bucket --key index.html --body index.html 
 aws s3 cp vendor/ s3://my-549763406380-bucket/vendor/ --recursive
 aws s3 cp css/ s3://my-549763406380-bucket/css/ --recursive
